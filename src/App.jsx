9cd35f9a527425597bb5e7442ca3105640ed0412
PawelJadach <pawel.jadach.96@gmail.com>
2020-04-03 13:47:09 +0200

    Initial project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainLayout from './MainLayout';
+
+const App = () => {
+  return (
+    <MainLayout>
+      
+    </MainLayout>
+  );
+}
+
+export default App;
---
+++ src/MainLayout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function MainLayout({children}) {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+MainLayout.propTypes = {
+  children: PropTypes.node,
+}
+
+export default MainLayout
+
