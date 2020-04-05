1c8039c8b44345a0bcdd49a1e2d1451087c99b72
PawelJadach <pawel.jadach.96@gmail.com>
2020-04-05 11:16:50 +0200

    Add about section

--- src/views/MainLayout.jsx
+++ src/views/MainLayout.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Navbar from '../components/Navbar/Navbar';
 import Header from '../components/Header/Header';
+import About from '../components/About/About';
 
 const  MainLayout = ({children}) => {
   return (
     <>
     <Navbar />
     <Header />
+    <About />
       {children}
     </>
   )
